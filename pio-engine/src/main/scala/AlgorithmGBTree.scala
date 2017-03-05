bcc7235bb34fc8eb490a22770f94129c93bfd38a
EatingW <yw428@cornell.edu>
2017-03-05 15:46:40 -0500

    gradientBoostedTree model

---
+++ pio-engine/src/main/scala/AlgorithmGBTree.scala
@@ -0,0 +1,48 @@
+import edu.cs5152.predictionio.demandforecasting._
+import grizzled.slf4j.Logger
+import org.apache.predictionio.controller.{CustomQuerySerializer, P2LAlgorithm}
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.clustering.KMeansModel
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LinearRegressionModel
+import org.apache.spark.mllib.tree.GradientBoostedTrees
+import org.apache.spark.mllib.tree.configuration.BoostingStrategy
+import org.apache.spark.mllib.tree.model.GradientBoostedTreesModel
+import org.joda.time.DateTime
+
+/**
+  * Created by YitingWang on 3/2/17.
+  */
+class AlgorithmGBTree extends P2LAlgorithm[PreparedData, ModelGBTree, Query, PredictedResult] with MyQuerySerializer{
+
+  override def train(sc: SparkContext, preparedData: PreparedData): ModelGBTree ={
+    val boostingStrategy = BoostingStrategy.defaultParams("Regression")
+    boostingStrategy.setNumIterations(3) // Note: Use more iterations in practice.
+    boostingStrategy.getTreeStrategy().setMaxDepth(5)
+    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+    boostingStrategy.getTreeStrategy().setCategoricalFeaturesInfo(Map[Int, Int]())
+
+    val gradientBoostedTreeModel = GradientBoostedTrees.train(preparedData.data, boostingStrategy)
+    new ModelGBTree(gradientBoostedTreeModel, Preparator.locationClusterModel.get)
+  }
+
+  override def predict(model: ModelGBTree, query: Query): PredictedResult = {
+    val label : Double = model.predict(query)
+    new PredictedResult(label)
+  }
+}
+
+class ModelGBTree(mod: GradientBoostedTreesModel, locationClusterModel: KMeansModel) extends Serializable { // will not be DateTime after changes
+// to Preparator
+@transient lazy val logger = Logger[this.type]
+
+  def predict(query: Query): Double = {
+    val locationClusterLabel = locationClusterModel.predict(Vectors.dense(query.lat, query.lng))
+    val features = Preparator.toFeaturesVector(DateTime.parse(query.eventTime), query.lat, query.lng, locationClusterLabel)
+    mod.predict(features)
+  }
+}
+
+trait MyQuerySerializer extends CustomQuerySerializer {
+  @transient override lazy val querySerializer = org.json4s.DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all
+}
--- pio-engine/src/main/scala/Engine.scala
+++ pio-engine/src/main/scala/Engine.scala
@@ -23,7 +23,7 @@ object ForecastingEngine extends EngineFactory {
     new Engine(
       classOf[DataSource],
       classOf[Preparator],
-      Map("alg" -> classOf[Algorithm]),
+      Map("alg" -> classOf[AlgorithmGBTree]),
       classOf[Serving])
   }
 }
