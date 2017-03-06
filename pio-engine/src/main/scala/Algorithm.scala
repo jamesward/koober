0bda8f28a864e10aa3d459b275b5a5e7e1c6ff9b
Siyuan Wang <wsylafa@gmail.com>
2017-03-06 12:56:30 -0500

    Implemented MultinomialLogisticRegression algorithm for predicting demand

--- pio-engine/src/main/scala/Algorithm.scala
+++ pio-engine/src/main/scala/Algorithm.scala
@@ -3,36 +3,46 @@ package edu.cs5152.predictionio.demandforecasting
 import grizzled.slf4j.Logger
 import org.apache.predictionio.controller.{CustomQuerySerializer, P2LAlgorithm, Params}
 import org.apache.spark.SparkContext
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.clustering.KMeansModel
+import org.apache.spark.mllib.feature.StandardScalerModel
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.optimization.SquaredL2Updater
 import org.apache.spark.mllib.regression.{LinearRegressionModel, LinearRegressionWithSGD}
 import org.joda.time.DateTime
 
 case class AlgorithmParams(
   iterations:        Int    = 20,
   regParam:          Double = 0.1,
-  miniBatchFraction: Double = 1.0, 
+  miniBatchFraction: Double = 1.0,
   stepSize:          Double = 0.001
 ) extends Params
 
-class Algorithm(val ap: AlgorithmParams)
+class MultinomialLogisticRegressionAlgorithm(val ap: AlgorithmParams)
   extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult] with MyQuerySerializer {
 
   @transient lazy val logger = Logger[this.type]
 
   def train(sc: SparkContext, preparedData: PreparedData): Model = {
-    val lin = new LinearRegressionWithSGD()
-    lin.setIntercept(true)
-    lin.setValidateData(true)
-    preparedData.data.collect().foreach(println)
-    lin.optimizer
-      .setNumIterations(50)
-      .setMiniBatchFraction(1.0)
-      .setStepSize(0.002)
-    val linearRegressionModel = lin.run(preparedData.data, Vectors.dense(0.2, 0.2, 0.5, 0.005, 0.1))
-//    print(linearRegressionModel.intercept)
-//    print(linearRegressionModel.weights)
-    new Model(linearRegressionModel, Preparator.locationClusterModel.get)
+    val logisticRegression = new LogisticRegressionWithLBFGS()
+
+    logisticRegression.setIntercept(true)
+    logisticRegression.setValidateData(true)
+    logisticRegression.setNumClasses(20) // Need to change when we have higher variations of demand levels
+    logisticRegression.optimizer
+      .setNumIterations(1000)
+      .setConvergenceTol(0.01)
+      .setUpdater(new SquaredL2Updater())
+      .setRegParam(0.5)
+
+// We can use the following sampling to reduce training set by sampling or increase training set by bootstrap
+    val sample = preparedData.data.sample(true, 0.01).cache();
+    sample.foreach(println)
+
+    val logisticRegressionModel = logisticRegression.run(preparedData.data)
+    println(logisticRegressionModel.intercept)
+    println(logisticRegressionModel.weights)
+    new Model(logisticRegressionModel, Preparator.locationClusterModel.get, Preparator.standardScalerModel.get)
   }
 
   def predict(model: Model, query: Query): PredictedResult = {
@@ -41,14 +51,14 @@ class Algorithm(val ap: AlgorithmParams)
   }
 }
 
-class Model(mod: LinearRegressionModel, locationClusterModel: KMeansModel) extends Serializable { // will not be DateTime after changes
-                                                                                  // to Preparator
+class Model(mod: LogisticRegressionModel, locationClusterModel: KMeansModel, standardScalerModel: StandardScalerModel) extends Serializable {
   @transient lazy val logger = Logger[this.type]
 
   def predict(query: Query): Double = {
+    val normalizedTimeFeatureVector = standardScalerModel.transform(Preparator.toFeaturesVector(DateTime.parse(query.eventTime), query.lat, query.lng))
     val locationClusterLabel = locationClusterModel.predict(Vectors.dense(query.lat, query.lng))
-    val features = Preparator.toFeaturesVector(DateTime.parse(query.eventTime), query.lat, query.lng, locationClusterLabel)
-    mod.predict(features)
+    val features = Preparator.toFeaturesVector(normalizedTimeFeatureVector, locationClusterLabel)
+    mod.predict((features))
   }
 }
 
--- pio-engine/src/main/scala/Preparator.scala
+++ pio-engine/src/main/scala/Preparator.scala
@@ -1,14 +1,11 @@
 package edu.cs5152.predictionio.demandforecasting
 
 
-import org.apache.predictionio.controller.PPreparator
-import org.apache.predictionio.controller.SanityCheck
-import edu.cs5152.predictionio.demandforecasting.KooberUtil
-
 import grizzled.slf4j.Logger
 import org.apache.predictionio.controller.{PPreparator, SanityCheck}
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.clustering.{KMeansModel, KMeans}
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.feature.{StandardScaler, StandardScalerModel}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
@@ -32,19 +29,31 @@ class Preparator extends PPreparator[TrainingData, PreparedData] {
   def prepare(sc: SparkContext, trainingData: TrainingData): PreparedData = {
 
     // clustering coordinates and assign cluster labels.
+    val standardScaler = new StandardScaler(true, true)//Used to calculate mean and std so that we can normalize features
     val locationData = trainingData.data map {entry => Vectors.dense(entry.lat, entry.lng)} distinct() cache()
-    val numClusters = 5
-    val numIterations = 20
+
     // store them statically so that we can use them when querying
-    Preparator.locationClusterModel = Some(KMeans.train(locationData, numClusters, numIterations))
+    Preparator.locationClusterModel = Some(KMeans.train(locationData, Preparator.numOfClustersForLocationModel,
+      Preparator.numOfIterationsForLocationModel))
 
     val normalizedTime = KooberUtil.createNormalizedMap(trainingData.data)
 
     val countMap = normalizedTime.values.countByValue()
     val normalizedTimeMap = normalizedTime.collectAsMap()
 
+    val featureVector = trainingData.data map { trainingDataEntry =>
+      Preparator.toFeaturesVector(trainingDataEntry.eventTime, trainingDataEntry.lat, trainingDataEntry.lng)
+    } cache ()
+
+    // store them statically so that we can normalize query data during query time
+    Preparator.standardScalerModel = Some(standardScaler.fit(featureVector))
+
     val data = trainingData.data map { trainingDataEntry =>
-      LabeledPoint(countMap.get(normalizedTimeMap.get(trainingDataEntry.eventTime).get).get, Preparator.toFeaturesVector(trainingDataEntry.eventTime, trainingDataEntry.lat, trainingDataEntry.lng))
+      val demand = countMap.get(normalizedTimeMap.get(trainingDataEntry.eventTime).get).get
+      val timeFeatureVector = Preparator.toFeaturesVector(trainingDataEntry.eventTime, trainingDataEntry.lat, trainingDataEntry.lng)
+      val normalizedTimeFeatureVector = Preparator.standardScalerModel.get.transform(timeFeatureVector)
+      val predictedLocationLabel = Preparator.locationClusterModel.get.predict(Vectors.dense(trainingDataEntry.lat, trainingDataEntry.lng))
+      LabeledPoint(demand, Preparator.toFeaturesVector(normalizedTimeFeatureVector, predictedLocationLabel))
     } cache ()
 
     new PreparedData(data)
@@ -55,19 +64,22 @@ object Preparator {
 
   @transient lazy val logger = Logger[this.type]
   var locationClusterModel: Option[KMeansModel] = None
+  var standardScalerModel: Option[StandardScalerModel] = None
+  val numOfClustersForLocationModel = 5
+  val numOfIterationsForLocationModel = 100
 
   def toFeaturesVector(eventTime: DateTime, lat: Double, lng: Double): Vector = {
-    toFeaturesVector(eventTime, lat, lng, Preparator.locationClusterModel.get.predict(Vectors.dense(lat, lng)).toDouble)
+    Vectors.dense(Array(
+      eventTime.dayOfWeek().get().toDouble,
+      eventTime.dayOfMonth().get().toDouble,
+      eventTime.minuteOfDay().get().toDouble,
+      eventTime.monthOfYear().get().toDouble
+    ))
   }
 
-  def toFeaturesVector(eventTime: DateTime, lat: Double, lng: Double, locationClusterLabel: Double): Vector = {
-    Vectors.dense(
-      Array(
-        eventTime.dayOfWeek().get().toDouble,
-        eventTime.dayOfMonth().get().toDouble,
-        eventTime.minuteOfDay().get().toDouble,
-        eventTime.monthOfYear().get().toDouble,
-        locationClusterLabel
-      ))
+  def toFeaturesVector(normalizedFeatureVector: Vector, locationClusterLabel: Int): Vector = {
+    val timeFeatures = normalizedFeatureVector.toArray
+    val locationFeatureOneHotEncoding = KooberUtil.convertIntToBinaryArray(locationClusterLabel, numOfClustersForLocationModel)
+    Vectors.dense(timeFeatures ++ locationFeatureOneHotEncoding)
   }
 }
