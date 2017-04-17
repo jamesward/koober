8d690abdd19c4ba6ba8087c791a67f61ac05db6b
EatingW <yw428@cornell.edu>
2017-04-16 22:31:40 -0400

    modified date and time slider to give dates and times. Right now would only change the on-screen value, but with some modifications the values can be turned into actual datatime.

--- demand-dashboard/public/analysis.js
+++ demand-dashboard/public/analysis.js
@@ -45,4 +45,22 @@ $(function() {
       zoom: 11,
       center: mapCenter
     });
+
+  document.getElementById('date-slider').addEventListener('input', function(e) {
+    document.getElementById('date-slider-value').textContent = "6/" + e.target.value.toString() + "/2017"
+  });
+
+  document.getElementById('time-slider').addEventListener('input', function(e) {
+    var input = parseInt(e.target.value);
+    document.getElementById('time-slider-value').textContent = (prettyNumbers(Math.floor(input/2)) + ":" + prettyNumbers((input%2)*30) + "-" +
+                                                              (prettyNumbers(Math.floor((input+1)/2))) + ":" + prettyNumbers(((input+1)%2)*30))
+  });
+
+  function prettyNumbers(number){
+    var result = number.toString()
+    if (result.length == 1){
+        return "0" + result
+    }
+    return result
+  }
 });
