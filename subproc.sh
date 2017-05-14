8e64eb1bd2d9d0745ac900d9be013d4784ba5c2d
James Ward <james@jamesward.org>
2017-05-13 22:30:40 -0600

    use bucketeer

--- subproc.sh
+++ subproc.sh
@@ -2,7 +2,7 @@
 
 if [ "$SUB_APP" = "pio-engine" ]; then
   echo "Downloading PredictionIO"
-  wget https://bucketeer-eb81b456-6374-4e94-82c5-5f8e82b7466b.s3.amazonaws.com/public/PredictionIO-0.11.0-incubating.zip
+  wget https://${BUCKETEER_BUCKET_NAME}.s3.amazonaws.com/public/PredictionIO-0.11.0-incubating.zip
   echo "Unzipping"
   unzip PredictionIO-0.11.0-incubating.zip
   PIO_HOME=~/PredictionIO-0.11.0-incubating
