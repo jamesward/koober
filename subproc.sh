98da57e279fd2f696f0466653ea91d16c2ff3769
James Ward <james@jamesward.org>
2017-05-14 21:39:22 -0600

    fix path

--- subproc.sh
+++ subproc.sh
@@ -7,5 +7,5 @@ if [ "$SUB_APP" = "pio-engine" ]; then
   unzip PredictionIO-0.11.0-incubating.zip
   PIO_HOME=~/PredictionIO-0.11.0-incubating
   cd pio-engine
-  $PIO_HOME/pio deploy --port $PORT
+  $PIO_HOME/bin/pio deploy --port $PORT
 fi
